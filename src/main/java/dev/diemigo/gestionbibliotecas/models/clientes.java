701ab9261a03b9fd1d6db64a2eb3bd5eba6e2b20
camicoptero <camil.castroh@duocuc.cl>
2026-04-17 12:11:07 -0400

    mi primer commint

---
+++ src/main/java/dev/diemigo/gestionbibliotecas/models/clientes.java
@@ -0,0 +1,24 @@
+package dev.diemigo.gestionbibliotecas.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class clientes {
+    @Id
+    @Column(unique = true)
+    private int id_clientes;
+    @Column(unique = true)
+    private String nom_clientes;
+    @Column(unique = true)
+    private String email_clientes;
+    @Column(unique = true)
+    private String rut_cliente;
+    private boolean activo;
+
+}
