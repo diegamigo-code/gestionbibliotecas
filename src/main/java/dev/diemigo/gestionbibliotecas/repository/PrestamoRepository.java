91b0c7b6ca819ac3f7ae1c37b841f35f6f817e60
camicoptero <camil.castroh@duocuc.cl>
2026-04-17 13:18:37 -0400

    mod PrestamoRepository

--- src/main/java/dev/diemigo/gestionbibliotecas/repository/PrestamoRepository.java
+++ src/main/java/dev/diemigo/gestionbibliotecas/repository/PrestamoRepository.java
@@ -1,4 +1,8 @@
 package dev.diemigo.gestionbibliotecas.repository;
+import dev.diemigo.gestionbibliotecas.models.Prestamo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+@Repository
 
-public interface PrestamoRepository {
+public interface PrestamoRepository extends JpaRepository<Prestamo, Integer> {
 }
