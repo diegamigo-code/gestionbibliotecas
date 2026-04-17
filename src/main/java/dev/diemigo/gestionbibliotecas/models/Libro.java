c426f32da8d44f1e4465a909307f3d648366f393
Renato Santander <re.santander@duocuc.cl>
2026-04-17 12:22:55 -0400

    2

--- src/main/java/dev/diemigo/gestionbibliotecas/models/Libro.java
+++ src/main/java/dev/diemigo/gestionbibliotecas/models/Libro.java
@@ -1,16 +1,27 @@
 package dev.diemigo.gestionbibliotecas.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-
-
-
-
-
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Libro {
-
+    @Id
+    @Column(unique = true)
+    private int id_libro;
+    @Column(unique = true)
+    private String nom_libro;
+    @Column(unique = true)
+    private String email_clientes;
+    @Column(unique = true)
+    private String rut_cliente;
+    private boolean valido;
 
 }
-
 
 
 
--- src/main/java/dev/diemigo/gestionbibliotecas/models/Prestamo.java
+++ src/main/java/dev/diemigo/gestionbibliotecas/models/Prestamo.java
@@ -1,9 +1,19 @@
 package dev.diemigo.gestionbibliotecas.models;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDate;
 
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Prestamo {
+
+
 
     private Integer idPrestamo;
     private Integer idLibro;
@@ -18,75 +28,4 @@ public class Prestamo {
     private Integer cantidadDias;
     private Integer multas;
 
-    public Prestamo() {
-        this.multas = 0;
-    }
-
-    public Prestamo(Integer idPrestamo, Integer idLibro, String runSolicitante,
-                    LocalDate fechaSolicitud, LocalDate fechaEntrega,
-                    Integer cantidadDias, Integer multas) {
-        this.idPrestamo = idPrestamo;
-        this.idLibro = idLibro;
-        this.runSolicitante = runSolicitante;
-        this.fechaSolicitud = fechaSolicitud;
-        this.fechaEntrega = fechaEntrega;
-        this.cantidadDias = cantidadDias;
-        this.multas = multas;
-    }
-
-    public Integer getIdPrestamo() {
-        return idPrestamo;
-    }
-
-    public void setIdPrestamo(Integer idPrestamo) {
-        this.idPrestamo = idPrestamo;
-    }
-
-    public Integer getIdLibro() {
-        return idLibro;
-    }
-
-    public void setIdLibro(Integer idLibro) {
-        this.idLibro = idLibro;
-    }
-
-    public String getRunSolicitante() {
-        return runSolicitante;
-    }
-
-    public void setRunSolicitante(String runSolicitante) {
-        this.runSolicitante = runSolicitante;
-    }
-
-    public LocalDate getFechaSolicitud() {
-        return fechaSolicitud;
-    }
-
-    public void setFechaSolicitud(LocalDate fechaSolicitud) {
-        this.fechaSolicitud = fechaSolicitud;
-    }
-
-    public LocalDate getFechaEntrega() {
-        return fechaEntrega;
-    }
-
-    public void setFechaEntrega(LocalDate fechaEntrega) {
-        this.fechaEntrega = fechaEntrega;
-    }
-
-    public Integer getCantidadDias() {
-        return cantidadDias;
-    }
-
-    public void setCantidadDias(Integer cantidadDias) {
-        this.cantidadDias = cantidadDias;
-    }
-
-    public Integer getMultas() {
-        return multas;
-    }
-
-    public void setMultas(Integer multas) {
-        this.multas = multas;
-    }
 }
