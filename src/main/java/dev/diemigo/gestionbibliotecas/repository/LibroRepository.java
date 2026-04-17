49155563dd86a179a4498c5c7d04bdc80c915f23
diego <dieg.amigo@duocuc.cl>
2026-04-17 12:12:16 -0400

    Interface Repo

---
+++ src/main/java/dev/diemigo/gestionbibliotecas/repository/LibroRepository.java
@@ -0,0 +1,9 @@
+package dev.diemigo.gestionbibliotecas.repository;
+
+import dev.diemigo.gestionbibliotecas.models.Libro;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LibroRepository extends JpaRepository<Libro, Integer> {
+}
