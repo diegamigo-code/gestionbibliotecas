6dd09efd9dc009134956a6be2f8fb4cdb99ee49b
diego <dieg.amigo@duocuc.cl>
2026-04-17 09:51:52 -0400

    Estado inicial (solo esqueleto)

---
+++ src/main/java/dev/diemigo/gestionbibliotecas/GestionbibliotecasApplication.java
@@ -0,0 +1,13 @@
+package dev.diemigo.gestionbibliotecas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionbibliotecasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionbibliotecasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/diemigo/gestionbibliotecas/GestionbibliotecasApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.diemigo.gestionbibliotecas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionbibliotecasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
