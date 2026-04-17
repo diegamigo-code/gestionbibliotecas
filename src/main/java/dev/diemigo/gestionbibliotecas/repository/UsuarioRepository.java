cf655359def11eb0ac3c6e590d908f177fb44086
camicoptero <camil.castroh@duocuc.cl>
2026-04-17 13:14:45 -0400

    add UsuarioRepository

---
+++ src/main/java/dev/diemigo/gestionbibliotecas/repository/UsuarioRepository.java
@@ -0,0 +1,7 @@
+package dev.diemigo.gestionbibliotecas.repository;
+import dev.diemigo.gestionbibliotecas.models.Usuario;
+import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+}
