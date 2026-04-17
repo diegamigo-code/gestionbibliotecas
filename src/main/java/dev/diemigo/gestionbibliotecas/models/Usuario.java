b798316b3da08224fa71de1676e6f1b9b5ad6986
camicoptero <camil.castroh@duocuc.cl>
2026-04-17 13:03:19 -0400

    e

--- src/main/java/dev/diemigo/gestionbibliotecas/models/Usuario.java
+++ src/main/java/dev/diemigo/gestionbibliotecas/models/Usuario.java
@@ -1,4 +1,24 @@
 package dev.diemigo.gestionbibliotecas.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Usuario {
+    @Id
+    @Column(unique = true)
+
+    private int id_usuario;
+    @Column(unique = true)
+    private String nom_usuario;
+    @Column(unique = true)
+    private String email_usuario;
+    @Column(unique = true)
+    private String rut_cliente;
+    private boolean valido;
+
 }
