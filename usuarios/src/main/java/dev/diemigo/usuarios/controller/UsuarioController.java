8584b15074650d83736ee70dd0f4813bae5ba18c
diego <dieg.amigo@duocuc.cl>
2026-05-07 14:49:08 -0400

    Try & Catch

--- usuarios/src/main/java/dev/diemigo/usuarios/controller/UsuarioController.java
+++ usuarios/src/main/java/dev/diemigo/usuarios/controller/UsuarioController.java
@@ -3,6 +3,7 @@ package dev.diemigo.usuarios.controller;
 
 import dev.diemigo.usuarios.dto.UsuarioRespuestaDTO;
 import dev.diemigo.usuarios.service.UsuarioService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("api/v1/gestionbibliotecas/")
+@RequestMapping("api/v1/usuarios")
 public class UsuarioController {
     @Autowired
     private UsuarioService usuarioService;
@@ -23,6 +24,44 @@ public class UsuarioController {
             return ResponseEntity.ok(usuarios);
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error en el servidor");
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<?> crearUsuario(@Valid @RequestBody UsuarioRespuestaDTO nuevoUsuario) {
+        try{
+            UsuarioRespuestaDTO usuario = usuarioService.crearUsuario(nuevoUsuario);
+            return ResponseEntity.status(HttpStatus.CREATED).body(usuario);
+        }catch(Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Hubo un error al intentar crear el usuario");
+        }
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<?> getUsuarioById(@PathVariable int id){
+        try{
+            return ResponseEntity.ok(usuarioService.buscarPorId(id));
+        }catch(Exception e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuario no encontrado");
+        }
+    }
+
+    @PutMapping
+    public ResponseEntity<?> actualizarUsuario(@PathVariable int id, @RequestBody UsuarioRespuestaDTO usuario){
+        try{
+            return ResponseEntity.ok(usuarioService.actualizarUsuario(id, usuario));
+        }catch(Exception e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuario no encontrado");
+        }
+    }
+
+    @DeleteMapping
+    public ResponseEntity<?> eliminarUsuario(@PathVariable int id){
+        try{
+            usuarioService.eliminarUsuario(id);
+            return null;
+        }catch(Exception e){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuario no encontrado");
         }
     }
 
--- usuarios/src/main/java/dev/diemigo/usuarios/model/Usuario.java
+++ usuarios/src/main/java/dev/diemigo/usuarios/model/Usuario.java
@@ -6,7 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Table(name = "Usuario")
+@Table(name = "Usuarios")
 @Entity
 @Data
 @AllArgsConstructor
