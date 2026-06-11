f34f0d3436834b1c97b2f5b51f22536f5c54ced6
camicoptero <camil.castroh@duocuc.cl>
2026-06-11 14:17:24 -0400

    inicio Swagger

---
+++ proveedores/src/main/java/data/proveedores/model/DTO/proveedorDTO.java
@@ -0,0 +1,22 @@
+package data.proveedores.model.DTO;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class proveedorDTO {
+    @NotNull
+    private String nombre_proveedor;
+
+    @NotNull
+    private String telefono_proveedor;
+    @NotNull
+    private String email_proveedor;
+    @NotNull
+    private String Descripcion;
+}
+
