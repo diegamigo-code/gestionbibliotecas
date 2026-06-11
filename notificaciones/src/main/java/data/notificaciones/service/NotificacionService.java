4fbca0f9066d322773bbbcb9cfb10dd2bd7b4e71
camicoptero <camil.castroh@duocuc.cl>
2026-06-11 14:17:51 -0400

    inicio Swagger

--- notificaciones/src/main/java/data/notificaciones/service/NotificacionService.java
+++ notificaciones/src/main/java/data/notificaciones/service/NotificacionService.java
@@ -1,11 +1,14 @@
 package data.notificaciones.service;
 
+import data.notificaciones.exception.NotFoundException;
 import data.notificaciones.model.Notificacion;
 import data.notificaciones.repository.NotificacionRepository;
 import data.notificaciones.model.DTO.DTONotificaciones;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.stereotype.Service;
 
+import java.util.stream.Collectors;
 import java.util.List;
 
 @Service
@@ -13,12 +16,25 @@ public class NotificacionService {
     @Autowired
     private NotificacionRepository repositoryNotificacion;
 
-    public List<Notificacion> obtenerNotificaciones() {return repositoryNotificacion.findAll();
-    }
-    public Notificacion getNotificaciones(int id){return repositoryNotificacion.findById(id).get();
+    private DTONotificaciones convertirDTO(Notificacion notificacion) {
+     return new DTONotificaciones(notificacion.getDia_entrega(),notificacion.getNotificacion_titulo(),notificacion.getDescripcion());
     }
 
-    public Notificacion addNotificacion(Notificacion modelNotificacion){
+    public List<DTONotificaciones> obtenerNotificaciones() {
+        return repositoryNotificacion.findAll()
+                .stream()
+                .map(this::convertirDTO)
+                .collect(Collectors.toList());
+    }
+
+
+    public DTONotificaciones getNotificacionesById (int id){
+        Notificacion notificacion = repositoryNotificacion.findById(id).orElseThrow(); new NotFoundException("");
+        return convertirDTO(notificacion);
+    }
+
+
+    public DTONotificaciones addNotificacion(Notificacion modelNotificacion){
         repositoryNotificacion.save(modelNotificacion);
         return modelNotificacion;
     }
